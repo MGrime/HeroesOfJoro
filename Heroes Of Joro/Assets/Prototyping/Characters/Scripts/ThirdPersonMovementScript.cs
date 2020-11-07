b3b4c694ced31061399276e29e632e9c926964f3
Michael Grime <MGrime1@uclan.ac.uk>
2020-11-07 10:57:23 +0000

    Refactored code to make it matching
    I havent changed any functionality. Just renamed variables and added some comments

--- Heroes Of Joro/Assets/Prototyping/Characters/Scripts/ThirdPersonMovementScript.cs
+++ Heroes Of Joro/Assets/Prototyping/Characters/Scripts/ThirdPersonMovementScript.cs
@@ -4,28 +4,60 @@ using UnityEngine;
 
 public class ThirdPersonMovementScript : MonoBehaviour
 {
-    public CharacterController mController;
-    public Transform PlayerCam;
-    public Animator mPlayerAnimation;
-    public float mSpeed = 10.0f;
-    public float mTurnSmootTime = 0.1f;
-    float mTurnSmoothVelocity;
+    #region Editor Fields
+
+    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private Transform _playerCamera;
+    [SerializeField] private Animator _playerAnimator;
+    [SerializeField] private float _moveSpeed = 10.0f;
+    [SerializeField] private float _turnSmoothTime = 0.1f;
+
+    #endregion
+
+    #region Private Data
+
+    private float _turnSmoothVelocity;
+
+    #endregion
+
+    #region Functions
+
     // Update is called once per frame
     void Update()
     {
-        float mHorizontal = Input.GetAxisRaw("Horizontal");
-        float mVertical = Input.GetAxisRaw("Vertical");
-        Vector3 mDirection = new Vector3(mHorizontal, 0.0f, mVertical).normalized;
-        if (mDirection.magnitude >= 0.1f)
+        // Get the input on both axis
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        // Calculate the movement direction
+        Vector3 direction = new Vector3(horizontal, 0.0f, vertical).normalized;
+
+        // If the direction is not null we are moving
+        if (direction.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(mDirection.x, mDirection.z)*Mathf.Rad2Deg+PlayerCam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref mTurnSmoothVelocity,mTurnSmootTime);
+            // Calculate the angle we will finish at
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _playerCamera.eulerAngles.y;
+
+            // Calculate angle from our current angle to the target angle whilst applying smoothing
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
+
+            // Set the new smoother angle
             transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
-            Vector3 mMoveDirection = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
-            mController.Move(mMoveDirection.normalized * mSpeed * Time.deltaTime);
-            mPlayerAnimation.SetBool("Moving", true);
+
+            // Move the model in the direction
+            Vector3 moveDirection = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
+            _characterController.Move(moveDirection.normalized * _moveSpeed * Time.deltaTime);
+
+            // Set the animation to play
+            _playerAnimator.SetBool("Moving", true);
         }
-        else mPlayerAnimation.SetBool("Moving", false);
+        else
+        {
+            // We aren't moving so stop the animation
+            _playerAnimator.SetBool("Moving", false);
+        }
 
     }
+
+    #endregion
 }
