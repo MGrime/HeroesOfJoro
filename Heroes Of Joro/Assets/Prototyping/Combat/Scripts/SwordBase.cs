49b8b940d28dc69d45484d141258d7e520bc530f
Michael Grime <MGrime1@uclan.ac.uk>
2020-12-28 14:21:18 +0000

    Zero animation warrior prefab
    it REALLY needs the animation to not look super scuffed

---
+++ Heroes Of Joro/Assets/Prototyping/Combat/Scripts/SwordBase.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordBase : MonoBehaviour
+{
+    #region Editor Fields
+    // Want this accessible by warrior
+    public bool _swinging;
+
+    [SerializeField] private float _attackDamage;
+    [SerializeField] private float _swingSpeed; // Degrees per frame
+
+    #endregion
+
+    #region Private Data
+
+    // So the way unity stores the data
+    // It goes from 45 -> 0 jumps to 360 -> 315
+    // Other way goes 315 -> 360 jumps to 0 -> 45
+    private bool _directionNegative;
+    private float _previousAngle = 0.0f;    // Store previous angle to find flip
+    private bool _flipped;
+
+    #endregion
+
+    #region Functions
+
+    void Start()
+    {
+        _swinging = false;
+        _directionNegative = true;
+        _flipped = false;
+        _previousAngle = transform.localRotation.eulerAngles.y;
+    }    
+    // Update is called once per frame
+    void Update()
+    {
+        if (_swinging)
+        {
+            float rotateAmount = _swingSpeed * Time.deltaTime;
+            if (_directionNegative) // 45 - > -45
+            {
+                rotateAmount *= -1.0f;  // Invert
+            }
+
+            _previousAngle = transform.localRotation.eulerAngles.y;
+            // Rotate
+            transform.Rotate(0.0f, rotateAmount, 0.0f);
+
+            // This is a cheap bodge catch all for when a big change happens
+            if (Mathf.Abs(transform.localRotation.eulerAngles.y - _previousAngle) > 180.0f)
+            {
+                _flipped = true;
+            }
+            
+            // Check if we hit swing time
+            // The numbers look funny because unity's interal holding of this number is scuffed
+            bool finished = false;
+            if (_directionNegative)
+            {
+                if (transform.localRotation.eulerAngles.y <= 315.0f && _flipped)
+                {
+                    finished = true;
+                }
+            }
+            else
+            {
+                if (transform.localRotation.eulerAngles.y >= 45.0f && _flipped)
+                {
+                    finished = true;
+                }
+            }
+
+            if (finished)
+            {
+                _swinging = false;
+                _directionNegative = !_directionNegative;
+                _flipped = false;
+            }
+        }
+    }
+
+    // Message functions
+    public void OnTriggerEnter(Collider other)
+    {
+        if (_swinging)
+        {
+            if (other.tag != "Player" && other.tag != "Spell")
+            {
+                if (other.name == "EnemyControls")
+                {
+                    other.gameObject.SendMessage("SetDamage", _attackDamage);
+                    other.gameObject.SendMessage("ReduceHealth");
+                }
+                if (other.name == "Face(Clone)")
+                {
+                    other.gameObject.SendMessage("ReduceProjectileHealth", _attackDamage);
+                }
+            }
+        }
+        
+    }
+
+    #endregion
+
+}
---
+++ Heroes Of Joro/Assets/Prototyping/Combat/Scripts/Warrior.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Warrior : PlayerBase
+{
+    #region Editor Fields
+
+    // Contains the camera and the 3D model
+    [SerializeField] private ThirdPersonMovementScript _physicalPlayer;
+
+    // Contains the sword object
+    [SerializeField] private SwordBase _sword;
+
+    // Data
+    [SerializeField] private float _maxHealth = 200.0f;
+
+    // UI
+    [SerializeField] private Slider _healthBar; 
+
+    #endregion
+
+    #region Private Data
+
+    // Store current health
+    private float _health;
+
+    #endregion
+
+    #region Functions
+
+    private void Update()
+    {
+        // Update bars
+        if (_healthBar)
+        {
+            _healthBar.value = _health / _maxHealth;
+        }
+
+        // Check inputs
+        // Left click
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            SwingSword();
+        }
+
+    }
+
+    private void SwingSword()
+    {
+        if (!_sword._swinging)
+        {
+            // This will trigger it in the sword function
+            _sword._swinging = true;
+        }
+    }
+
+
+    // Message functions
+
+    public void ReceiveDamage(int damage)
+    {
+        if (_health > 0)
+        {
+            _health -= damage;
+            Debug.Log("Damage received:" + damage);
+
+        }
+        else if (_health <= 0)
+        {
+            //Destroy(gameObject);
+            //TO DO: change this so it sends a message to GameOver function in the GameManager class
+        }
+    }
+    public void PickupHealth()
+    {
+        if (_health < _maxHealth - 50.0f)
+        {
+            _health += 50.0f;
+        }
+        else
+        {
+            _health = _maxHealth;
+        }
+    }
+    #endregion
+}
