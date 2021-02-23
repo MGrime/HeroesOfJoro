aa2332d7396df0dcd3e9ec0d932af0fe33bae1e9
Michael Grime <MGrime1@uclan.ac.uk>
2021-02-23 14:05:43 +0000

    Hub room with portal
    portal takes you to dungeon

---
+++ Heroes Of Joro/Assets/Prototyping/Hub/Scripts/HubPortal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HubPortal : MonoBehaviour
+{
+    public string _mainGameScene;
+    public AudioSource _teleportSound;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            return;
+        }
+
+        if (_teleportSound)
+        {
+            _teleportSound.ignoreListenerVolume = true;
+            _teleportSound.Play();
+        }
+        SceneManager.LoadScene(_mainGameScene);
+    }
+}
