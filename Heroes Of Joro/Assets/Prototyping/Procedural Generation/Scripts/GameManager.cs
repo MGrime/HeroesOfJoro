7f1a3dca67abb6af6a7cff066624fe3f8503b907
Michael Grime <MGrime1@uclan.ac.uk>
2021-04-27 14:48:30 +0100

    Update GameManager.cs
    upgrades linked in

--- Heroes Of Joro/Assets/Prototyping/Procedural Generation/Scripts/GameManager.cs
+++ Heroes Of Joro/Assets/Prototyping/Procedural Generation/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -72,6 +73,81 @@ public class GameManager : MonoBehaviour
                 _coinCounterText.text = _sessionCoins.ToString() + '(' + _coinsAtStart + ')';
             }
         }
+        // Get component on the same game object that stores the player references
+        var playerManager = GetComponentInParent<PlayerManager>();
+
+        // got the reference
+        if (playerManager)
+        {
+            // Get the array of player trackers
+            var playerArray = playerManager.PlayerTrackers;
+
+            foreach (var player in playerArray)
+            {
+                // Switch off the type
+                switch (player.Type)
+                {
+                    case PlayerBase.PlayerType.Mage:
+                    {
+                        // convert to type
+                        var mage = (Mage) player;
+
+                        // Read in values, default values are the standards 
+                        var maxHealth = PlayerPrefs.GetFloat("Mage_MaxHealth", mage.MaxHealth);
+                        var specialUpgradeCount = PlayerPrefs.GetInt("Mage_SpecialUpgradeCount", 0);
+
+                        // Update values
+                        mage.MaxHealth = maxHealth;
+                        mage.MaxMana += 20 * specialUpgradeCount;
+
+                        // Print to check
+                        Debug.Log("Mage health: " + mage.MaxHealth + " Mana: " + mage.MaxMana);
+
+                        break;
+                    }
+                    case PlayerBase.PlayerType.Warrior:
+                    {
+                        // Convert to type
+                        var warrior = (Warrior) player;
+
+                        // Read in values, default values are the standards
+                        var maxHealth = PlayerPrefs.GetFloat("Warrior_MaxHealth", warrior.MaxHealth);
+                        var specialUpgradeCount = PlayerPrefs.GetInt("Warrior_SpecialUpgradeCount", 0);
+
+                        // Update values
+                        warrior.MaxHealth = maxHealth;
+                        warrior.SwordDamage += 5 * specialUpgradeCount;
+
+                        // Print to check
+                        Debug.Log("Warrior health: " + warrior.MaxHealth + " Sword: " + warrior.SwordDamage);
+
+                        break;
+                    }
+                    case PlayerBase.PlayerType.Archer:
+                    {
+                        // Convert to type
+                        var archer = (ArcherBetter) player;
+
+                        // Read in values, default values are the standarsd
+                        var maxHealth = PlayerPrefs.GetFloat("Archer_MaxHealth", archer.MaxHealth);
+                        var specialUpgradeCount = PlayerPrefs.GetInt("Archer_SpecialUpgradeCount", 0);
+
+                        // Update values
+                        archer.MaxHealth = maxHealth;
+                        archer.BowSpeed -= 0.15f * specialUpgradeCount;
+
+                        // Print to check
+                        Debug.Log("Archer health: " + archer.MaxHealth + " Bow speed: " + archer.BowSpeed);
+
+                        break;
+                    }
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+        }
+
 
     }
 
